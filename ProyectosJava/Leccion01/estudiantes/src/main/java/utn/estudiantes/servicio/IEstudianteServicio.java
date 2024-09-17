65b0c8c0d85ab353446d7c56e11e23b025e10928
Maxi Echegaray <maceech7@gmail.com>
2024-09-17 11:33:36 -0300

    Subiendo Aplicacion Estudiantes

--- ProyectosJava/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
+++ ProyectosJava/Leccion01/estudiantes/src/main/java/utn/estudiantes/servicio/IEstudianteServicio.java
@@ -1,13 +1,13 @@
 package utn.estudiantes.servicio;
 
-import utn.estudiantes.modelo.Estudiante;
+import utn.estudiantes.modelo.Estudiantes2023;
 
 import java.util.List;
 
 public interface IEstudianteServicio {
-    public List<Estudiante> listarEstudiantes();
-    public Estudiante buscarEstudiantePorId(Integer idEstudiante);
-    public void guardarEstudiante(Estudiante estudiante);
-    public void eliminarEstudiante(Estudiante estudiante);
+    public List<Estudiantes2023> listarEstudiantes();
+    public Estudiantes2023 buscarEstudiantePorId(Integer idEstudiantes2023);
+    public void guardarEstudiante(Estudiantes2023 estudiante);
+    public void eliminarEstudiante(Estudiantes2023 estudiante);
 
 }
