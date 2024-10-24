66b86abad15ff93ece7d5a0a15ed564588bbc356
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-10-23 22:10:53 -0300

    Agregar estudiante

--- Metodologia/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
+++ Metodologia/Leccion12/SistemaEstudiantes/src/main/java/UTN/datos/EstudianteDAO.java
@@ -1,7 +1,6 @@
 package UTN.datos;
 
 import UTN.dominio.Estudiante;
-
 import static UTN.conexxion.Conexion.getConnection;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
@@ -76,6 +75,32 @@ public class EstudianteDAO {
         return false;
     }
 
+    //Metodo agregar un nuevo estudiante
+    public boolean agregarEstudiante(Estudiante estudiante){
+        PreparedStatement ps;
+        Connection con = getConnection();
+        String sql = "INSERT INTO estudiantes2024 (nombre, apellido, telefono, email) VALUES (?, ?, ? ,?)";
+        try {
+            ps=con.prepareStatement(sql);
+            ps.setString(1,estudiante.getNombre());
+            ps.setString(2,estudiante.getApellido());
+            ps.setString(3,estudiante.getTelefono());
+            ps.setString(4,estudiante.getEmail());
+            ps.execute();
+            return true;
+        } catch (Exception e) {
+            System.out.println("Ocurrio un error al agregar el estudiante: "+e.getMessage());
+        }//fin del catch
+        finally {
+            try{
+                con.close();
+            }catch (Exception e){
+                System.out.println("Error al cerrar la conexion: "+e.getMessage());
+            }//fin catch
+        }//fin finaly
+        return false;
+    }//fin metodo agregar estudiante
+
     public static  void main(String[] args){
         //Listar los estudiantes
         var estudianteDao= new EstudianteDAO();
@@ -83,8 +108,23 @@ public class EstudianteDAO {
         List<Estudiante>estudiantes=estudianteDao.listarEstudiantes();
         estudiantes.forEach(System.out::println);//Funcion lamba para imprimir
 
+        //Agregar estudiante
+        var nuevoEstudiante= new Estudiante("Carlos","Lara","621651986","carlosl@mail.com");
+        var agregado= estudianteDao.agregarEstudiante(nuevoEstudiante);
+        if(agregado)
+            System.out.println("Estudiante agregado: "+nuevoEstudiante);
+        else 
+            System.out.println("No se ha agregado estudiante: "+nuevoEstudiante);
+
+
         //Buscar por id
-        
+        //var estudiante1= new Estudiante(1);
+        //System.out.println("Estudiantes antes de la busqueda: "+estudiante1);
+        //var encontrado= estudianteDao.buscarEstudiantePorId(estudiante1);
+        //if(encontrado)
+        //    System.out.println("Estudiante encontrado: "+estudiante1);
+        //else
+        //    System.out.println("No se encontro el estudiante: "+estudiante1.getIdEstudiante());
 
     }
 }
