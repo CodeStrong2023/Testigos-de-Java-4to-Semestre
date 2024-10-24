9c35c6cce3c23d6a067cf8f0fff7f2e5276aa42c
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-10-24 16:34:35 -0300

    Metodo buscar por id

--- ProyectosJava/Leccion02/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
+++ ProyectosJava/Leccion02/estudiantes/src/main/java/utn/estudiantes/repositorio/EstudianteRepositorio.java
@@ -1,4 +1,8 @@
 package utn.estudiantes.repositorio;
 
-public interface EstudianteRepositorio {
+import org.springframework.data.jpa.repository.JpaRepository;
+import utn.estudiantes.modelo.Estudiante;
+
+
+public interface EstudianteRepositorio extends JpaRepository<Estudiante, Integer> {
 }
