d13d36c0992733b80a34088f04c2caf6f255f52a
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-10-29 11:03:40 -0300

    Terminamos proyecto de estudiantes con spreen

---
+++ ProyectosJava/Leccion02/estudiantes/src/main/java/java/sql/SQLSyntaxErrorExceptionImplImpl.java
@@ -0,0 +1,10 @@
+package java.sql;
+
+import java.util.Spliterator;
+
+public class SQLSyntaxErrorExceptionImplImpl extends SQLSyntaxErrorExceptionImpl {
+    @Override
+    public Spliterator<Throwable> spliterator() {
+        return super.spliterator();
+    }
+}
