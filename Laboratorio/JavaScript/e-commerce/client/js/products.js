e3e90a8ffb24278ff52b20416947f9b6e1b1fbc3
Nicolas <nicolasezequielgutierrez@gmail.com>
2024-11-11 12:47:29 -0300

    subiendo archivos faltantes

---
+++ Laboratorio/JavaScript/e-commerce/client/js/products.js
@@ -0,0 +1,37 @@
+const productos = [
+    {
+        id: 1,
+        productName: "Banana",
+        price: 420,
+        quanty: 1,
+        img: "/media/banana.jpg"
+    },
+    {
+        id: 2,
+        productName: "Leche",
+        price: 950,
+        quanty: 1,
+        img: "/media/leche.jpg"
+    },
+    {
+        id: 3,
+        productName: "Pollo",
+        price: 780,
+        quanty: 1,
+        img: "/media/pollo.jpg"
+    },
+    {
+        id: 4,
+        productName: "Mayonesa",
+        price: 510,
+        quanty: 1,
+        img: "/media/mayonesa.jpg"
+    },
+    {
+        id: 5,
+        productName: "Enlatados",
+        price: 520,
+        quanty: 1,
+        img: "/media/enlatados.jpg"
+    }
+]
