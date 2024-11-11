9b8cc82aef98fad8beb43237ded8a4edf6683579
NicoChara <nicolaschiaraviglio70@gmail.com>
2024-11-11 01:27:46 -0300

    subo carpetas tecnicatura4

---
+++ proyecto practicas/pps/app/views.py
@@ -0,0 +1,94 @@
+from django.shortcuts import render, HttpResponse
+from django.views.generic import ListView, FormView, View, DeleteView
+from django.urls import reverse, reverse_lazy
+from .models import Room, Booking
+from .forms import AvailabilityForm
+from app.booking_functions.availability import check_availability
+
+# Create your views here.
+
+def RoomListView(request):
+    room = Room.objects.all()[0]
+    room_categories = dict(room.ROOM_CATEGORIES)
+    print('categories=', room_categories)
+    
+    room_values = room_categories.values()
+    print('categories=', room_values)
+    room_list = []
+    
+    for room_category in room_categories:
+        room = room_categories.get(room_category)
+        room_url = reverse('app:RoomDetailView', kwargs=
+                           {'category': room_category})
+        
+        room_list.append((room, room_url))     
+    context={
+        'room_list': room_list,
+    }
+    return render(request, 'index.html', context)
+    
+class BookingListView(ListView):
+    model = Booking
+    template_name="booking_list_view.html"
+    def get_queryset(self, *args, **kwargs):
+        if self.request.user.is_staff:
+            booking_list = Booking.objects.all()
+            return booking_list
+        else:
+            booking_list = Booking.objects.filter(user=self.request.user)
+            return booking_list
+        
+    
+class RoomDetailView(View):
+    def get(self, request, *args, **kwargs):
+        category = self.kwargs.get('category', None)
+        form = AvailabilityForm()
+        room_list = Room.objects.filter(category=category)
+        
+        if len(room_list) > 0:        
+            room = room_list[0]
+            room_category = dict(room.ROOM_CATEGORIES).get(room.category, None)
+            context={
+                'room_category': room_category,
+                'form': form,
+            }
+            return render(request, 'room_detail_view.html', context)
+        else:
+            return HttpResponse('Category no existe')
+        
+        
+        
+        
+    def post(self, request, *args, **kwargs):
+        category = self.kwargs.get('category', None)
+        room_list = Room.objects.filter(category=category)
+        form = AvailabilityForm(request.POST)
+
+        if form.is_valid():
+            data = form.cleaned_data
+
+        available_rooms = []
+        for room in room_list:
+            if check_availability(room, data['check_in'], data['check_out']):
+                available_rooms.append(room)
+
+                
+        if len(available_rooms)>0:
+            room = available_rooms[0]
+            
+            booking = Booking.objects.create(
+                user = self.request.user,
+                room = room,
+                check_in = data['check_in'],
+                check_out = data['check_out']
+            )
+            booking.save()
+            return HttpResponse(booking)
+        else:
+            return HttpResponse('Esta categoria de habitaciones está reservada, prueba otra')
+        
+class CancelBookingView(DeleteView):
+    model = Booking
+    template_name = 'booking_cancel_view.html'
+    success_url = reverse_lazy('app:BookingListView')
+        
